6f62617e9aa2af8f781e64af2024c222ffa2557c
Valerria Hutsuliak <valeria.gutsuliak@gmail.com>
2019-10-03 21:44:53 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,22 @@
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each do |item|
+    if new_hash[item.keys[0]]
+      new_hash[item.keys[0]][:count] += 1
+    else
+      new_hash[item.keys[0]] = {
+        count: 1,
+        price: item.values[0][:price],
+        clearance: item.values[0][:clearance]
+      }
+    end
+  end
+  new_hash
+end	
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  
 end
 
 def apply_clearance(cart)
