ad175d146af914704785520b5243d413efb93a1a
Valerria Hutsuliak <valeria.gutsuliak@gmail.com>
2019-10-03 21:46:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,7 +49,6 @@ def checkout(cart, coupons)
   consol_cart = consolidate_cart(cart)
   cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
   cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
-
   total = 0.0
   cart_with_discounts_applied.keys.each do |item|
     total += cart_with_discounts_applied[item][:price]*cart_with_discounts_applied[item][:count]
